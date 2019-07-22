c7da0c257c8f900c075ca08466712966bffbc748
unknown <h.nezamabadi@nezamabadi-7.pooyaco.ir>
2019-07-22 13:39:01 +0430

    instagram

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'report',
+        loadChildren: './report/report.module#ProjectNewReportModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
